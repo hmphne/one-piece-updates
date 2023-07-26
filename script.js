0a2ae6226c46ee32e5ba3c0c86ad3e676e3015aa
hmphne <3rdoperson@gmail.com>
2023-07-26 10:14:45 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let latestChapeter;
+
+async function latestChapter() {
+  const res = await fetch("https://api.api-onepiece.com/chapters/count");
+  const record = await res.json();
+  console.log(record);
+  latestChapeter = record
+  document.getElementById("chapter_num").innerHTML = record;
+
+  const resp = await fetch(`https://api.api-onepiece.com/chapters/${latestChapeter}`);
+  const records = await resp.json();
+  console.log(records)
+  document.getElementById("chapter_title").innerHTML = records.chapter_title;
+
+}
+latestChapter();
+
+console.log(latestChapeter)
